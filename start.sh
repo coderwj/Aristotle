4b00975899dce413a9e2d554293f4601c58798bb
coderwj <1019139374@qq.com>
2016-11-29 01:18:49 +0800

    add start.sh for linux

---
+++ start.sh
@@ -0,0 +1,16 @@
+sudo apt-get install mongo
+
+echo 'install mongo finish.'
+
+sudo apt-get update
+
+sudo apt-get install nodejs
+sudo apt-get install npm
+
+echo 'install nodejs finish.'
+
+sudo apt-get update
+
+echo 'start app---------------'
+nodejs ./app.js
+echo 'start app finish---------------'
